287f69741efae633b3c6bcc8ea1adefa675d98bc
bdurands <79468011+bdurands@users.noreply.github.com>
2026-08-12 00:13:18 -0500

    HYPER TELEPORT ROCK 2

--- src/main/java/net/server/channel/handlers/HyperTeleportRockHandler.java
+++ src/main/java/net/server/channel/handlers/HyperTeleportRockHandler.java
@@ -4,10 +4,15 @@ import client.Client;
 import net.AbstractPacketHandler;
 import net.packet.InPacket;
 import server.maps.HyperTeleportRockService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class HyperTeleportRockHandler extends AbstractPacketHandler {
+    private static final Logger log = LoggerFactory.getLogger(HyperTeleportRockHandler.class);
+
     @Override
     public void handlePacket(InPacket p, Client c) {
+        log.info("Received HYPER_TELEPORT_ROCK packet! targetMapId bytes available: " + p.available());
         if (p.available() < Integer.BYTES) {
             HyperTeleportRockService.enableActions(c);
             return;
