2b14520d9d70aa01e4091aa120028bd1ecf562ae
bdurands <79468011+bdurands@users.noreply.github.com>
2026-08-12 21:49:45 -0500

    HYPER TELEPORT ROCK FINAL
    - Agregamos al cash shop como donor x1 x3 x7 dias

--- scripts/npc/hyperteleportrock.js
+++ scripts/npc/hyperteleportrock.js
@@ -12,23 +12,23 @@ function action(mode, type, selection) {
         cm.dispose();
         return;
     }
-    
+
     var targetMapId = cm.getPlayer().getHyperTeleportRockTargetMapId();
     if (targetMapId <= 0) {
         cm.dispose();
         return;
     }
-    
+
     var mapFactory = cm.getClient().getChannelServer().getMapFactory();
     var targetMap = mapFactory.getMap(targetMapId);
-    
+
     if (targetMap == null) {
         cm.dispose();
         return;
     }
-    
+
     if (status == 0) {
-        cm.sendYesNo("¿Estás seguro de que quieres viajar a #b" + targetMap.getMapName() + "#k?");
+        cm.sendYesNo("Estas seguro de que quieres viajar a #b" + targetMap.getMapName() + "#k?");
     } else if (status == 1) {
         cm.dispose();
         Packages.server.maps.HyperTeleportRockService.tryTeleport(cm.getClient(), targetMapId, true);
