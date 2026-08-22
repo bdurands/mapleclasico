bdb8164d5ac8d9a21519b7119565176cf16f25d4
bdurands <79468011+bdurands@users.noreply.github.com>
2026-08-22 11:50:19 -0500

    prueba 3

--- scripts/npc/9906601.js
+++ scripts/npc/9906601.js
@@ -1,10 +1,5 @@
 var status = 0;
 
-// En lugar de usar ítems (que pueden verse muy grandes), usamos ASCII limpio y logos oficiales de la UI.
-var arrow = "»"; 
-var star = "★";
-var nxIcon = "#fUI/CashShop.img/CSCoin/0#"; // El logo oficial de NX del Cash Shop
-
 function start() {
     status = -1;
     action(1, 0, 0);
@@ -26,16 +21,16 @@ function action(mode, type, selection) {
         var bossPoints = cm.getPlayer().getBossPoints();
         var donorPoints = cm.getPlayer().getCashShop().getCash(2);
 
-        var text = "\t\t\t#d#e" + star + " Centro de Puntos EllinMS " + star + "#n#k\r\n\r\n";
+        var text = "\t\t\t#d#e Centro de Puntos EllinMS #n#k\r\n\r\n";
         text += "¡Hola, #b#h ##k! Aquí tienes el resumen de tus puntos acumulados. ¡Sigue así!\r\n\r\n";
         
-        text += "  " + arrow + " #e#bVotación:#n#k \t\t" + votePoints + " Puntos\r\n";
-        text += "  " + arrow + " #e#dParty Quest:#n#k \t" + pqPoints + " Puntos\r\n";
-        text += "  " + arrow + " #e#rBoss Points:#n#k \t" + bossPoints + " Puntos\r\n";
-        text += "  " + arrow + " #e#gDonador:#n#k \t\t" + donorPoints + " Puntos\r\n\r\n";
+        text += "  - #e#bVotación:#n#k \t\t" + votePoints + " Puntos\r\n";
+        text += "  - #e#dParty Quest:#n#k \t" + pqPoints + " Puntos\r\n";
+        text += "  - #e#rBoss Points:#n#k \t" + bossPoints + " Puntos\r\n";
+        text += "  - #e#bDonador:#n#k \t\t" + donorPoints + " Puntos\r\n\r\n"; // Cambiado a azul (#b) para evitar el verde chillón
         
         text += "#b------------------------------------------------------#k\r\n";
-        text += "#L0# " + nxIcon + " #eCanjear#n 10 Puntos de Votación por #b5,000 NX Credit#k#l\r\n";
+        text += "#L0##eCanjear#n 10 Puntos de Votación por #b5,000 NX Credit#k#l\r\n";
 
         cm.sendSimple(text);
     } else if (status == 1) {
@@ -44,7 +39,7 @@ function action(mode, type, selection) {
             if (votePoints >= 10) {
                 cm.getClient().useVotePoints(10);
                 cm.getPlayer().getCashShop().gainCash(1, 5000); // 1 = NX Credit
-                cm.sendOk(nxIcon + " ¡Felicidades! Has canjeado #r10 Puntos de Votación#k por #b5,000 NX Credit#k.\r\n\r\n¡Disfrútalos en el Cash Shop!");
+                cm.sendOk("¡Felicidades! Has canjeado #r10 Puntos de Votación#k por #b5,000 NX Credit#k.\r\n\r\n¡Disfrútalos en el Cash Shop!");
                 cm.dispose();
             } else {
                 cm.sendOk("Lo siento, no tienes suficientes #b10 Puntos de Votación#k.\r\n\r\nActualmente tienes: #r" + votePoints + "#k.");
