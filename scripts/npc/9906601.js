5c7c8e99af633d30644c0084dd03adf7d47e750d
bdurands <79468011+bdurands@users.noreply.github.com>
2026-08-22 11:46:30 -0500

    PRUEBA ICONOS NPC

--- scripts/npc/9906601.js
+++ scripts/npc/9906601.js
@@ -1,5 +1,11 @@
 var status = 0;
 
+var voteIcon = "#v4000313#"; // Hoja de Arce Dorada
+var pqIcon = "#v4001158#";   // Lazo de Confianza (PQ)
+var bossIcon = "#v4001086#"; // Certificado de Zakum
+var donorIcon = "#v5200002#"; // Tarjeta de 10k NX
+var nxIcon = "#v5200001#";   // Tarjeta de 5k NX
+
 function start() {
     status = -1;
     action(1, 0, 0);
@@ -16,23 +22,21 @@ function action(mode, type, selection) {
     }
 
     if (status == 0) {
-        // Obtenemos los puntos disponibles nativamente en tu source (Kaentake)
         var votePoints = cm.getClient().getVotePoints();
         var pqPoints = cm.getPlayer().getPqPoints();
-        
-        // Boss points ahora implementados en el Source
         var bossPoints = cm.getPlayer().getBossPoints();
-        
-        // Donor Points = Maple Points (Tipo 2 en el CashShop)
         var donorPoints = cm.getPlayer().getCashShop().getCash(2);
 
-        var text = "¡Hola #b#h ##k! Aquí tienes un resumen de tus puntos actuales:\r\n\r\n";
-        text += "#ePuntos de Votación:#n " + votePoints + "\r\n";
-        text += "#ePuntos de Party Quest (PQ):#n " + pqPoints + "\r\n";
-        text += "#ePuntos de Boss:#n " + bossPoints + "\r\n";
-        text += "#ePuntos de Donador:#n " + donorPoints + "\r\n\r\n";
-        text += "------------------------------------------------------\r\n";
-        text += "#L0##bCanjear 10 Puntos de Votación por 5,000 NX Credit#k#l\r\n";
+        var text = "\t\t\t\t#d#e ¡Centro de Puntos EllinMS! #n#k\r\n\r\n";
+        text += "¡Hola, #b#h ##k! Aquí tienes el resumen detallado de tus puntos acumulados. ¡Sigue así!\r\n\r\n";
+        
+        text += " " + voteIcon + " #e#bVotación:#n#k \t\t" + votePoints + " Puntos\r\n";
+        text += " " + pqIcon + " #e#dParty Quest:#n#k \t" + pqPoints + " Puntos\r\n";
+        text += " " + bossIcon + " #e#rBoss Points:#n#k \t" + bossPoints + " Puntos\r\n";
+        text += " " + donorIcon + " #e#gDonador:#n#k \t\t" + donorPoints + " Puntos\r\n\r\n";
+        
+        text += "#b------------------------------------------------------#k\r\n";
+        text += "#L0# " + nxIcon + " #eCanjear#n 10 Puntos de Votación por #b5,000 NX Credit#k#l\r\n";
 
         cm.sendSimple(text);
     } else if (status == 1) {
@@ -41,10 +45,10 @@ function action(mode, type, selection) {
             if (votePoints >= 10) {
                 cm.getClient().useVotePoints(10);
                 cm.getPlayer().getCashShop().gainCash(1, 5000); // 1 = NX Credit
-                cm.sendOk("¡Felicidades! Has canjeado #r10 Puntos de Votación#k por #b5,000 NX Credit#k. ¡Disfrútalos!");
+                cm.sendOk(nxIcon + " ¡Felicidades! Has canjeado #r10 Puntos de Votación#k por #b5,000 NX Credit#k.\r\n\r\n¡Disfrútalos en el Cash Shop!");
                 cm.dispose();
             } else {
-                cm.sendOk("Lo siento, no tienes suficientes Puntos de Votación. Necesitas al menos 10 y actualmente tienes #r" + votePoints + "#k.");
+                cm.sendOk(voteIcon + " Lo siento, no tienes suficientes #b10 Puntos de Votación#k.\r\n\r\nActualmente tienes: #r" + votePoints + "#k.");
                 cm.dispose();
             }
         }
